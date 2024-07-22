cd60ce6394e917c349d09b1dfb3c8b61a3b6f2c8
QuocGiangDL <ptquocgiang2710@gmail.com>
2024-07-22 17:57:52 +0700

    up

--- mstbt/index.php
+++
@@ -1,3 +0,0 @@
-<?php include 'style/header.php';?>
-<div class="p-2 m-md-3 rounded shadow-sm bg-white">Chào buổi chiều!<br>Chúc một ngày làm việc vui vẻ <3 </div>
-<?php include 'style/footer.php';?>
